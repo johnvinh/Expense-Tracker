5e33c72476f511b3ef761768740557c1ca0dd2a6
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-16 21:28:15 -0500

    added extra comments

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ function App()
 {
         const [expenses, setExpenses] = useState([]);
 
+        // Load in existing expenses from localstorage
         useEffect(() =>
         {
                 setExpenses(JSON.parse(localStorage.getItem('expenses') as string));
         }, []);
 
+        // Update localstorage whenever a new expense is added
         useEffect(() =>
         {
                 localStorage.setItem('expenses', JSON.stringify(expenses));
